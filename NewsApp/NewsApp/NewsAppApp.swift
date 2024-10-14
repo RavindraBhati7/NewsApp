17deb8205cc48db53c5565abe39666598f670e7d
Ravindra <bhati.ravindra919@gmail.com>
2024-10-14 19:12:22 +0530

    Project Foundation

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Ravindra bhati on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
